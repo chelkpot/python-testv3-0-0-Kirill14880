7df1c1b35ffbfc7b40e7565fb085366510601c68
Kirill <gusenokki2007@gmail.com>
2025-10-12 12:11:30 +0500

    Add Python tasks solutions

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,11 +1,13 @@
 # tasks/task2.py
 
 def solve():
-# Ниже пишите решение задачи
+    # Ниже пишите решение задачи
+    n = int(input())
+    n = n % 1440
+    hours = n // 60
+    minutes = n % 60
+    print(hours, minutes)
 
-   
-
-   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
